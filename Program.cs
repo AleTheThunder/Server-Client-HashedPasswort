3557cedbe77d7a1e7cf3fd7a80cf196c7e230271
Alexander Donner <donneralexander22@gmx.at>
2023-11-01 20:49:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(IPAddress.Parse("127.0.0.1"), 8080);
+
+                NetworkStream stream = client.GetStream();
+
+                string password = "meinPasswort";
+                string hashedPassword = HashPassword(password);
+                byte[] passwordBytes = Encoding.ASCII.GetBytes(hashedPassword);
+
+                stream.Write(passwordBytes, 0, passwordBytes.Length);
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Server antwortet: {response}");
+
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
